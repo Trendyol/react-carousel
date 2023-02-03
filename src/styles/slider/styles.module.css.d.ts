a6a130e5f5f098f4c3f93bea7caa31bec7f1c24f
Burcu Saglam <burcu.saglam@trendyol.com>
2023-02-03 15:12:05 +0300

    chore: Remove unnecessary style property

--- src/styles/slider/styles.module.css.d.ts
+++ src/styles/slider/styles.module.css.d.ts
@@ -1,10 +1,8 @@
 export const sliderBase: string;
 export const slider: string;
-export const sliding: string;
 interface Namespace {
 	sliderBase: string;
 	slider: string;
-	sliding: string;
 }
 declare const stylesModule: Namespace;
 export default stylesModule;
